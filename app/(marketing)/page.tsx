b790bb3b51b1a034b8133f76eca6300cf5ad5676
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2024-04-17 15:51:24 +0000

    marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+import { Heading } from "./_components/heading";
+import {Heroes} from "./_components/heroes";
+import { Footer } from "./_components/footer"
+
+const MarketingPage = () => {
   return (
-    <p>Marketing Page</p>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
+    </div>
   );
 }
+
+export default MarketingPage;
