70a00b938ce24abc5fad5982aa21096025806119
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2024-04-18 19:22:53 +0000

    auth ready to be implemented

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export const ConexClientProvider = ({ children }: { children: ReactNode }) => {
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
