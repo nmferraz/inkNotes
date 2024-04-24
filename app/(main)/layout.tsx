b8a3e1fcb3a1ef2e80774c7de82acbec0c873f76
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2024-04-24 18:03:06 +0000

    Update dependencies and add SearchCommand component to MainLayout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,35 +1,39 @@
 "use client";
 
 import { useConvexAuth } from "convex/react";
-import { Spinner } from "@/components/spinner";
 import { redirect } from "next/navigation";
+
+import { Spinner } from "@/components/spinner";
+import { SearchCommand } from "@/components/search-command";
+
 import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
-    children
+  children
 }: {
-        children: React.ReactNode;
+  children: React.ReactNode;
 }) => {
-    const { isAuthenticated, isLoading } = useConvexAuth();
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-    if (isLoading) {
-        return(
-            <div className="h-full flex items-center justify-center">
-                <Spinner size="lg" />
-            </div>
-        )
-    }
-
-    if(!isAuthenticated) {
-        return redirect("/");
-    }
-
+  if (isLoading) {
     return (
-        <div className="h-full flex dark:bg-[#1F1F1F]">
-            <Navigation />
-            <main className="flex-1 h-full overflow-y-auto">{children}</main>
-        </div>
-    )
-}
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
 
-export default MainLayout;
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return ( 
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
+    </div>
+   );
+}
